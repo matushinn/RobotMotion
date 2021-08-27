2cf347ec1f949b23c3cc59f3bbab0cea00c2ddc0
sho <44314610+matushinn@users.noreply.github.com>
2021-08-27 17:44:55 +0900

    Update RobotMotion.cs

--- Assets/Scripts/RobotMotion.cs
+++ Assets/Scripts/RobotMotion.cs
@@ -1,26 +1,34 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-public class RobotMotion
-{
+
+public class RobotMotion {
+
+    //メソッドをオブジェクトとして保存するActionクラス(RobotScript(モーション対象のロボット),float(モーションが始まってからの時間))
     Action<RobotScript, float> animation;
-    float pastTime;
-    float duration=0;
+    //何秒かかるか
+    float duration;
+    //現在の時間
+    float pastTime = 0;
 
-    public RobotMotion(RobotMotion src) : this(src.animation, src.duration)
+    //コピー
+    public RobotMotion(RobotMotion src) : this(src.animation,src.duration)
     {
-
     }
-    public RobotMotion(Action<RobotScript, float> animation, float duration )
+    public RobotMotion(Action<RobotScript, float> animation, float duration)
     {
-        this.animation = animation; //上のと区別している
+        //上のとの区別
+        this.animation = animation;
         this.duration = duration;
     }
-    public bool Animate(RobotScript robot,float deltaTime)
+    //モーションが動くかの確認の関数
+    public bool Animate(RobotScript robot, float deltaTime)
     {
         pastTime += deltaTime;
-        animation(robot,pastTime/duration);
+        //割合として引数で渡す
+        animation(robot, pastTime / duration);
+        //アニメーションが終わったのか？
         return pastTime >= duration;
     }
 }
